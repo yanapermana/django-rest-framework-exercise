4fb3320c8f5b07fa07cbe668f7e2d4eb2b5cd991
yanapermana <kumpoter@gmail.com>
2017-08-11 10:37:40 +0700

    Whatever

---
+++ part-3/class-based/tutorial/tutorial/urls.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.conf.urls import url, include
+
+urlpatterns = [
+    url(r'^', include('snippets.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+]
